f5c38e40545952cd6aeed0f30a026ee97b07857b
mohammad <mohammednawas8@gmail.com>
2022-10-17 15:40:44 +0300

    Tool bar setup

--- app/src/main/java/com/example/productsadder/MainActivity.kt
+++ app/src/main/java/com/example/productsadder/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.productsadder
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import com.example.productsadder.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
     }
 }
