c079da599172eab2d26f05317d21710ee266b17b
mohammad <mohammednawas8@gmail.com>
2022-10-17 16:47:02 +0300

    Before uploading

--- app/src/main/java/com/example/productsadder/MainActivity.kt
+++ app/src/main/java/com/example/productsadder/MainActivity.kt
@@ -1,18 +1,78 @@
 package com.example.productsadder
 
+import android.app.Activity
+import android.content.Intent
+import android.content.Intent.ACTION_GET_CONTENT
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.productsadder.databinding.ActivityMainBinding
+import com.skydoves.colorpickerview.ColorEnvelope
+import com.skydoves.colorpickerview.ColorPickerDialog
+import com.skydoves.colorpickerview.listeners.ColorEnvelopeListener
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-
+    val selectedColors = mutableListOf<Int>()
+    var selectedImages = mutableListOf<Uri>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        //4
+        binding.buttonColorPicker.setOnClickListener {
+            ColorPickerDialog
+                .Builder(this)
+                .setTitle("Product color")
+                .setPositiveButton("Select", object : ColorEnvelopeListener {
+
+                    override fun onColorSelected(envelope: ColorEnvelope?, fromUser: Boolean) {
+                        envelope?.let {
+                            selectedColors.add(it.color)
+                            updateColors()
+                        }
+                    }
+
+                }).setNegativeButton("Cancel") { colorPicker, _ ->
+                    colorPicker.dismiss()
+                }.show()
+        }
+
+
+        val selectImagesActivityResult =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == Activity.RESULT_OK) {
+                    val intent = result.data
+
+                    //Multiple images selected
+                    if (intent?.clipData != null) {
+                        val count = intent.clipData?.itemCount ?: 0
+                        (0 until count).forEach {
+                            val imagesUri = intent.clipData?.getItemAt(it)?.uri
+                            imagesUri?.let { selectedImages.add(it) }
+                        }
+
+                        //One images was selected
+                    } else {
+                        val imageUri = intent?.data
+                        imageUri?.let { selectedImages.add(it) }
+                    }
+                    updateImages()
+                }
+            }
+        //6
+        binding.buttonImagesPicker.setOnClickListener {
+            val intent = Intent(ACTION_GET_CONTENT)
+            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+            intent.type = "image/*"
+            selectImagesActivityResult.launch(intent)
+        }
+
+
     }
 
 
@@ -22,6 +82,55 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //1
+        if (item.itemId == R.id.saveProduct) {
+            val productValidation = validateInformation()
+            if (!productValidation) {
+                Toast.makeText(this, "Check your inputs", Toast.LENGTH_SHORT).show()
+                return false
+            }
+            saveProducts()
+        }
         return super.onOptionsItemSelected(item)
     }
+
+    //2
+    private fun validateInformation(): Boolean {
+        if (selectedImages.isEmpty())
+            return false
+        if (binding.edName.text.toString().trim().isEmpty())
+            return false
+        if (binding.edCategory.text.toString().trim().isEmpty())
+            return false
+        if (binding.edPrice.text.toString().trim().isEmpty())
+            return false
+        return true
+    }
+
+    //3
+    private fun saveProducts() {
+        val sizes = getSizesList(binding.edSizes.text.toString().trim())
+    }
+
+    private fun getSizesList(sizes: String): List<String>? {
+        if (sizes.isEmpty())
+            return null
+        val sizesList = sizes.split(",").map { it.trim() }
+        return sizesList
+    }
+
+    //5
+    private fun updateColors() {
+        var colors = ""
+        selectedColors.forEach {
+            colors = "$colors ${Integer.toHexString(it)}, "
+        }
+        binding.tvSelectedColors.text = colors
+    }
+
+    private fun updateImages(){
+        binding.tvSelectedImages.setText(selectedImages.size.toString())
+    }
+
+
 }
